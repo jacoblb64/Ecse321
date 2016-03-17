81c7e8a036cb3c1a026658dcadbfd4205f0d2e40
Jacob B <jacoblb64@gmail.com>
2016-03-17 12:58:18 -0400

    started testing

--- src/A4/Q2Test.java
+++ src/A4/Q2Test.java
@@ -5,10 +5,22 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class Q2Test {
-
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Integer[] testnums = {5, 3, 7, 5, 9};
+		
+		Integer[] unpairednums = Q2.findUnpairedNumbers(testnums);
+		
+		Integer[] correct = {3, 7, 9};
+		
+		// check the lengths are the same
+		assertEquals(correct.length, unpairednums.length);
+		
+		// check all elements match
+		for (int i = 0; i < unpairednums.length; i++) {
+			assertEquals(correct[i], unpairednums[i]);
+		}
 	}
 
 }
